b4ba93a39d023ce21d7f88ad60c529122ec12734
Luis Pablo <luismpablo@gmail.com>
2016-08-21 14:15:26 -0400

    cleanup

--- router.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(controller) {
-  controller.hears('(testing(?: )?)+', ['direct_message'], function(bot, message){
-    bot.reply(message, 'I hear ya, loud and clear!')
-  })
-
-}
